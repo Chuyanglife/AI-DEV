0182f0b0d34e4e11d66badb649129bbc44ea232f
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-03 22:09:45 +0800

    vault backup: 2025-09-03 22:09:45

--- demo.py
+++ demo.py
@@ -2,17 +2,17 @@ class scmp:
     def __init__(self):
         pass
     
-    def 
-    while True:
-        s1=input("s1: ")
-        if s1=="0":
-            break
-        s2=input("s2: ")
-        if s2=="0":
-            break
-        #print("string: "+s1+s2)
-        if len(s1)>=len(s2):
-            print("string: "+s1+s2)
-        if len(s2)>=len(s1):
-            print("string: "+s2+s1)
-    print("end")
+    def run(self):
+        while True:
+            s1=input("s1: ")
+            if s1=="0":
+                break
+            s2=input("s2: ")
+            if s2=="0":
+                break
+            #print("string: "+s1+s2)
+            if len(s1)>=len(s2):
+                print("string: "+s1+s2)
+            if len(s2)>=len(s1):
+                print("string: "+s2+s1)
+        print("end")
