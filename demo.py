15bc66e5e22137d53a370cb486f484ffcea9a606
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-13 21:27:23 +0800

    vault backup: 2025-09-13 21:27:23

--- demo.py
+++ demo.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
-s = map(chr,input().split())
-#print(' '.join(f"{ord(c):02X}" for c in s))
+s = input().split()
 
-# 方法1: 转换为列表打印
-print(list(s))
+print(s)
 
-# 方法2: 如果要重新使用map对象，需要重新创建
-# s = map(chr,input().split())
-# print(''.join(s))
