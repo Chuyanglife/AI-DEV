d9d02ce3ec4ce62f76a0e858fba1bfc5cca31563
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-22 19:37:27 +0800

    vault backup: 2025-09-22 19:37:27

--- demo.py
+++ demo.py
@@ -17,8 +17,8 @@ def digits_or_letters(s:str)->str:
         return "tie"
 
 print(digits_or_letters("abc123"))
-digits_or_letters("a1b2c3d"))
-digits_or_letters("1a2b3c4")
-digits_or_letters("abc123!@#DEF")
-digits_or_letters("H3110 W0R1D")
-digits_or_letters("P455W0RD")
+print(digits_or_letters("a1b2c3d"))
+digits_or_letters("1a2b3c4"))
+digits_or_letters("abc123!@#DEF"))
+digits_or_letters("H3110 W0R1D"))
+digits_or_letters("P455W0RD"))
