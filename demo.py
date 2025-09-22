5bf9ae15b62d674cbdc256282f0b0e2d5564785e
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-22 19:34:50 +0800

    vault backup: 2025-09-22 19:34:50

--- demo.py
+++ demo.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
-import socket
+def digits_or_letters(s:str)->str:
+    digits,letters=0,0
 
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
-
-while True:
-    data = mysock.recv(512)
-    if len(data) < 1:
-        break
-    print(data.decode(),end='')
-mysock.close()
+    for c in s:
+        if c.isdigit():
+            digits+=1
+        elif c.isalpha():
+            letters+=1
+    
+    if digits>letters:
+        return "letters"
+    elif letters>digits:
+        return "digits"
+    else:
+        return "tie"
