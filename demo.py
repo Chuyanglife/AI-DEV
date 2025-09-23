dc26dcee43c893e98e4e0ac462a13bcde5b49db2
Chuyanglife <40868137+Chuyanglife@users.noreply.github.com>
2025-09-23 20:51:36 +0800

    vault backup: 2025-09-23 20:51:36

--- demo.py
+++ demo.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
-x=list()
-print(type(x))
-print(dir(x)
+def is_mirror(s1: str, s2: str) -> bool:
+    f1 = ""
+    for ch in s1:
+        if ch.isalpha():
+            f1 += ch
+
+    f2 = ""
+    for ch in s2:
+        if ch.isalpha():
+            f2 += ch
+
+    return f1 == f2[::-1]
+
+# Ê¾Àý²âÊÔ
+print(is_mirror("abc", "cba"))              # True
+print(is_mirror("Abc!", "cba"))             # False
+print(is_mirror("A man, a plan!", "nalp a ,nam A"))  # True
+print(is_mirror("123abc", "cba456"))        # True
